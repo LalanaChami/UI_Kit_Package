77d8e1ccfb73c97fbb62fb6ab5e869a287ac362c
Lalana <t.lalana@theglobalmarketaccess.com>
2023-11-30 09:19:44 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UI_Kit_Package",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "UI_Kit_Package",
+            targets: ["UI_Kit_Package"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "UI_Kit_Package"),
+        .testTarget(
+            name: "UI_Kit_PackageTests",
+            dependencies: ["UI_Kit_Package"]),
+    ]
+)
