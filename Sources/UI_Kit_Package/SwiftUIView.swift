8cd8b6343f0733cf39d01e06e58beb88b599539b
Lalana <t.lalana@theglobalmarketaccess.com>
2023-11-30 10:55:08 +0530

    Custom View changes

---
+++ Sources/UI_Kit_Package/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Amila Manathunga on 2023-11-30.
+//
+
+import SwiftUI
+
+public struct SwiftUIView: View {
+    @available(iOS 13.0.0, *)
+    public var body: some View {
+        Text("Hello, GTN!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
