2577a6ef9eb834f3fe1f43bdfc85f604c1f82666
Lalana <t.lalana@theglobalmarketaccess.com>
2023-12-04 13:54:42 +0530

    Adding currency changes and the UI changes

--- Sources/UI_Kit_Package/PotfolioCardView.swift
+++ Sources/UI_Kit_Package/PotfolioCardView.swift
@@ -14,34 +14,98 @@ public struct PotfolioCardView: View {
     public var holdingAmount : String
     public var netChangeAmount : String
     public var netChangePercentage : String
+    public var currency : String
     
-    public init(headingTitile: String, icon: String, holdingAmount: String, netChangeAmount: String, netChangePercentage: String) {
+    public init(headingTitile: String, icon: String, holdingAmount: String, netChangeAmount: String, netChangePercentage: String, currency: String) {
         self.headingTitile = headingTitile
         self.icon = icon
         self.holdingAmount = holdingAmount
         self.netChangeAmount = netChangeAmount
         self.netChangePercentage = netChangePercentage
+        self.currency = currency
       }
     
     @available(iOS 13.0, *)
     public var body: some View {
         
         ZStack {
-            HStack {
+            VStack(alignment: .leading, spacing: 20) {
                 Text("\(headingTitile)")
                     .font(
                         Font.custom("Inter", size: 20)
                             .weight(.semibold)
                     )
-                .foregroundColor(.black)
+                    .foregroundColor(Color(red: 0.3176, green: 0.102, blue: 0.349))
+//                    .border(Color.red)
+                
+                HStack {
+                    ZStack {
+                        Image(systemName: "dollarsign.square.fill")
+                            .resizable()
+                            .foregroundColor(Color(red: 0.6039, green: 0.4314, blue: 0.6275))
+                            .frame(width: 20, height: 20)
+                    }
+                    .frame(width: 50, height: 50)
+                    .background(Color(red: 0.9137, green: 0.8275, blue: 0.9294))
+                    .cornerRadius(12)
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color(red: 0.9, green: 0.9, blue: 0.9), lineWidth: 0)
+                    )
+                    
+                    Text("\(holdingAmount)")
+                        .font(
+                            Font.custom("Inter", size: 30)
+                                .weight(.heavy)
+                        )
+                        .foregroundColor(Color(red: 0.6039, green: 0.4314, blue: 0.6275))
+                    Text( " " + "\(currency)")
+                        .font(
+                            Font.custom("Inter", size: 20)
+                                .weight(.heavy)
+                        )
+                        .frame(alignment: .center)
+                        .foregroundColor(Color(red: 0.6039, green: 0.4314, blue: 0.6275))
+                }
+//                .border(Color.red)
+                
+                HStack(alignment: .center, spacing: 10) {
+                    Image(systemName: "chart.line.uptrend.xyaxis")
+                        .resizable()
+                        .foregroundColor(Color(red: 0.6039, green: 0.4314, blue: 0.6275))
+                        .frame(width: 16, height: 15)
+                    
+                    Text("\(netChangeAmount)")
+                        .font(
+                            Font.custom("Inter", size: 15)
+                                .weight(.heavy)
+                        )
+                        .foregroundColor(Color(red: 0.6039, green: 0.4314, blue: 0.6275))
+                    
+                    Text("(" + "\(netChangePercentage)" + ")")
+                        .font(
+                            Font.custom("Inter", size: 15)
+                                .weight(.heavy)
+                        )
+                        .foregroundColor(Color(red: 0.6039, green: 0.4314, blue: 0.6275))
+                }
+                .frame(width: .infinity, height: 20)
+                .padding(.top, 20)
+                .padding(.leading, 10)
+//                .border(Color.red)
+                
+                
             }
+            .frame(minWidth: 0, maxWidth: 260, minHeight: 0, maxHeight: 150, alignment: .topLeading)
+//            .border(Color.red)
+
         }
         .frame(width: 300, height: 200)
-        .background(Color(red: 0.98, green: 0.98, blue: 0.96))
+        .background(Color(red: 0.9608, green: 0.9294, blue: 0.9686))
         .cornerRadius(24)
         .overlay(
         RoundedRectangle(cornerRadius: 24)
-        .stroke(Color(red: 0.9, green: 0.9, blue: 0.9), lineWidth: 2)
+        .stroke(Color(red: 0.9, green: 0.9, blue: 0.9), lineWidth: 0)
 
         )
         
@@ -53,7 +117,7 @@ struct PotfolioCardView_Previews: PreviewProvider {
     @available(iOS 13.0.0, *)
     static var previews: some View{
         Group {
-        PotfolioCardView(headingTitile: "Portfolio", icon: "holding-icon", holdingAmount: "120,000" , netChangeAmount: "+23.44", netChangePercentage: "23%")
+            PotfolioCardView(headingTitile: "Portfolio Value", icon: "holding-icon", holdingAmount: "120,000" , netChangeAmount: "+23.44", netChangePercentage: "23%", currency: "AED")
                 .previewLayout(.sizeThatFits)
             
         }
