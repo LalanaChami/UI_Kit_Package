55f3e49e653cf18516e9971201daae607efc5b8b
Lalana <t.lalana@theglobalmarketaccess.com>
2023-12-12 11:20:56 +0530

    Adding unit test for testing Portfolio View

--- Tests/UI_Kit_PackageTests/Portfolio/PotfolioCardViewUITests.swift
+++ Tests/UI_Kit_PackageTests/Portfolio/PotfolioCardViewUITests.swift
@@ -2,34 +2,29 @@
 //  PotfolioCardViewUITests.swift
 //  
 //
-//  Created by Amila Manathunga on 2023-12-12.
+//  Created by Lalana Chamika on 2023-12-12.
 //
 
+import SwiftUI
 import XCTest
+import UI_Kit_Package
 
+@available(iOS 13.0, *)
 final class PotfolioCardViewUITests: XCTestCase {
+    
+    func testPotfolioCardViewRendering() {
+            let view = PotfolioCardView(
+                headingTitile: "Portfolio Value",
+                icon: "holding-icon",
+                holdingAmount: "120,000",
+                netChangeAmount: "+23.44",
+                netChangePercentage: "23%",
+                currency: "AED"
+            )
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+            let hostingController = UIHostingController(rootView: view)
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+            XCTAssertNotNil(hostingController.view, "View should be rendered")
     }
 
 }
