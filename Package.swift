3b6908c65366ed1ce74f1b19c9dd1a223cbefc7e
Lalana <t.lalana@theglobalmarketaccess.com>
2024-01-12 11:25:31 +0530

    NME-72 | NME-137 - Integrate localization and language support - iOS

--- Package.swift
+++ Package.swift
@@ -15,7 +15,12 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UI_Kit_Package"),
+            name: "UI_Kit_Package",
+            resources: [
+                .copy("Sources/Localization/en.json"),
+                .copy("Sources/Localization/ar.json"),
+            ]
+        ),
         .testTarget(
             name: "UI_Kit_PackageTests",
             dependencies: ["UI_Kit_Package"]),
